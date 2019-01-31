7b612c4fc62e72877f25ae4a19c68d7d124e7295
Nicole D <nnardella@gmail.com>
2019-01-31 17:53:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
 
       @pirates = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
+      params[:pirate][:ships].each do |details| binding.pry
         Ship.new(details)
       end
 
